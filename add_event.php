1e634c2ee25f8da1178ea24d591da4a714571db5
yondesta <yonnny1234@gmail.com>
2025-07-24 12:09:01 +0300

    event

--- add_event.php
+++ add_event.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection details - IMPORTANT: Replace with your actual credentials
 define('DB_SERVER', 'localhost'); // Usually 'localhost'
-define('DB_USERNAME', 'yonas'); // Your database username
+define('DB_USERNAME', 'biruktzw_yonas'); // Your database username
 define('DB_PASSWORD', 'Biruk@123'); // Your database password
-define('DB_NAME', 'biruktransport'); // The database name you created
+define('DB_NAME', 'biruktzw_biruktransport'); // The database name you created
 
 // Attempt to connect to MySQL database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
