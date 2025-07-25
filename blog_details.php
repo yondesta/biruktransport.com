beb36c1ce29f00f845f6cdb561526e23eaf0d74c
yondesta <yonnny1234@gmail.com>
2025-07-25 18:53:49 +0300

    event modify

--- blog_details.php
+++ blog_details.php
@@ -179,9 +179,9 @@ $conn->close();
                 <h2>Comments</h2><hr>
                 <?php if (!empty($comments)): ?>
                     <?php foreach ($comments as $comment): ?>
-                        <div class="comment">
-                            <strong><?php echo htmlspecialchars($comment['commenter_name']); ?></strong>
-                            <span class="comment-date"><?php echo date('F j, Y, g:i a', strtotime($comment['created_at'])); ?></span>
+                        <div class="image-text">
+                            <h6><strong><?php echo htmlspecialchars($comment['commenter_name']); ?></strong>
+                            <span><?php echo date('F j, Y, g:i a', strtotime($comment['created_at'])); ?></span></h6>
                             <p><?php echo nl2br(htmlspecialchars($comment['comment_text'])); ?></p>
                         </div>
                     <?php endforeach; ?>
@@ -193,20 +193,36 @@ $conn->close();
             <div class="comments-area">
                 <h3>Leave a Comment</h3>
                 <div class="message"><?php echo $comment_message; ?></div>
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . '?id=' . htmlspecialchars($event['id']); ?>" method="post">
-                    <input type="hidden" name="event_id" value="<?php echo htmlspecialchars($event['id']); ?>">
-
-                    <label for="commenter_name">Your Name:</label>
-                    <input type="text" id="commenter_name" name="commenter_name" value="<?php echo isset($_POST['commenter_name']) ? htmlspecialchars($_POST['commenter_name']) : ''; ?>" required>
-
-                    <label for="commenter_email">Your Email (optional):</label>
-                    <input type="email" id="commenter_email" name="commenter_email" value="<?php echo isset($_POST['commenter_email']) ? htmlspecialchars($_POST['commenter_email']) : ''; ?>">
-
-                    <label for="comment_text">Your Comment:</label>
-                    <textarea id="comment_text" name="comment_text" required><?php echo isset($_POST['comment_text']) ? htmlspecialchars($_POST['comment_text']) : ''; ?></textarea>
-
-                    <button type="submit" name="submit_comment">Post Comment</button>
+            <div class="form-area">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . '?id=' . htmlspecialchars($event['id']); ?>" class="default-form" method="post">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <input type="hidden" name="event_id" value="<?php echo htmlspecialchars($event['id']); ?>">
+                       
+                      <input type="text" class="form-control" placeholder="Your Name" id="commenter_name" name="commenter_name" value="<?php echo isset($_POST['commenter_name']) ? htmlspecialchars($_POST['commenter_name']) : ''; ?>" required>
+                    </div>
+                  </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    
+                    <input type="email" id="commenter_email" class="form-control email" placeholder="Your Email (optional)" name="commenter_email" value="<?php echo isset($_POST['commenter_email']) ? htmlspecialchars($_POST['commenter_email']) : ''; ?>">
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="form-group">
+                    
+                    <textarea id="comment_text" name="comment_text" class="form-control textarea" placeholder="Your Comment" required><?php echo isset($_POST['comment_text']) ? htmlspecialchars($_POST['comment_text']) : ''; ?></textarea>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="form-group form-bottom">
+                    <button type="submit" class="btn-style-one" name="submit_comment">Post Comment</button>
+                  </div>
+                </div>
+                </div>
                 </form>
+             </div>
             </div>
 
             <a href="blog.php" class="back-link">← Back to All Events</a>
@@ -215,85 +231,8 @@ $conn->close();
          
           </div>
         </div>
-        <div class="comments-area">
-          <div class="sec-title">
-            <h6>Comments (3)</h6>
-          </div>
-          <div class="comment">
-            <div class="image-holder">
-              <figure>
-                <img loading="lazy" src="images/blog/1.png" alt="">
-              </figure>
-            </div>
-            <div class="image-text">
-              <h6>Jacky Chan<span>April 06, 2020</span></h6>
-              <h5><a href="blog-details.php">
-                <i class="fa fa-reply-all" aria-hidden="true"></i>Comment Back
-              </a></h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam sequi praesentium, veritatis nulla
-                quam at exercitationem? Nesciunt molestiae neque modi.</p>
-            </div>
-          </div>
-          <div class="comment reply-comment">
-            <div class="image-holder">
-              <figure>
-                <img loading="lazy" src="images/blog/2.png" alt="">
-              </figure>
-            </div>
-            <div class="image-text">
-              <h6>Robben Oti<span>April 07, 2020</span></h6>
-              <h5><a href="blog-details.php">
-                <i class="fa fa-reply-all" aria-hidden="true"></i>Comment Back
-              </a></h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, a! Lorem ipsum dolor, sit amet
-                consectetur adipisicing elit. Excepturi, obcaecati!</p>
-            </div>
-          </div>
-          <div class="comment">
-            <div class="image-holder">
-              <figure>
-                <img loading="lazy" src="images/blog/3.png" alt="">
-              </figure>
-            </div>
-            <div class="image-text">
-              <h6>Chang Lingma<span>May 12, 2020</span></h6>
-              <h5><a href="blog-details.php">
-                <i class="fa fa-reply-all" aria-hidden="true"></i>Comment Back
-              </a></h5>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae possimus suscipit esse quia
-                sapiente obcaecati consequuntur dolor sequi adipisci unde!</p>
-            </div>
-          </div>
-        </div>
-        <div class="form-area">
-          <div class="sec-title">
-            <h6>Leave A Comment</h6>
-          </div>
-          <form id="contact_form" name="contact_form" class="default-form">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="form-group">
-                  <textarea name="form_message" class="form-control textarea" placeholder="Your Review" required></textarea>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <input type="text" name="form_name" class="form-control" placeholder="Your Name" required>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <input type="email" name="form_email" class="form-control email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="form-group form-bottom">
-                  <button class="btn-style-one" type="submit">Comment</button>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
+        
+        
       </div>
       <div class="col-lg-3">
         <div class="right-side">
